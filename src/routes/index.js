438e7de1f3155b4479a4b26f58d3625b27495334
BriMelany <briyitmelanycapcha@gmail.com>
2025-02-08 10:52:12 -0500

    Agregar login,inicio,reportes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const router=Router();
 
 
 router.get('/', (req,res) => res.render('index',{title:'Inicio'}))
-router.get('/clientes', (req,res) => res.render('clientes',{title:'Menu'}))
+router.get('/inicio', (req,res) => res.render('inicio',{title:'Productos'}))
+router.get('/clientes', (req,res) => res.render('clientes',{title:'Cliente'}))
+router.get('/login', (req,res) => res.render('login',{title:'login'}))
+router.get('/reportes', (req,res) => res.render('reportes',{title:'reportes'}))
 
 
 export default router;
